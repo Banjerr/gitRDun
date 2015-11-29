b3e7c4c1078d2e5881b2b914d7c83322aa0ac981
Ben Redden <benjamminredden@gmail.com>
2015-11-29 13:54:17 -0500

    changes to eloquent-oauth.php [deploy: production]

--- config/eloquent-oauth.php
+++ config/eloquent-oauth.php
@@ -6,7 +6,7 @@ return [
 		'github' => [
 			'client_id' => '37bb75932ff23a375233',
 			'client_secret' => '6b0c74359fc0464c0c44fe556748dc9391817db3',
-			'redirect_uri' => 'http://gitrdun.countryfriedcoders.me',
+			'redirect_uri' => 'http://gitrdun.countryfriedcoders.me/github/login',
 			'scope' => [],
 		],
 	],

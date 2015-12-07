afe9fc01788b00daaf067fc0028d7dcb30bb8d26
Ben Redden <benjamminredden@gmail.com>
2015-12-07 11:04:38 -0500

    messing around with oAuth stuff [deploy: production]

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -7,13 +7,8 @@
     <h2>- Coming Soon -</h2>
     <h2><a class="icon-gunShowCat" target="_blank" href="//github.com/banjerr/gitrdun">GitHub</a></h2>
     <!-- TODO set up in the right way -->
-    <a class="authenticateYourself" href="/github/authorize">Click here to get started</a>
+    <a class="authenticateYourself" href="/github/login">Click here to get started</a>
 </section><!--.comingSoon-->
-
-@if( ! empty( $user ) )
-    // echo get code
-    <p>{{$user->name}}</p>
-@endif
 
 <!-- Create Task Form... -->
 

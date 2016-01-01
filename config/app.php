8092cdd39ffd6c1f6daa092d58ebfa1d97130657
Ben Redden <benjamminredden@gmail.com>
2016-01-01 16:10:19 -0500

    weird stuff happening [deploy: production]

--- config/app.php
+++ config/app.php
@@ -149,7 +149,7 @@ return [
         /*
          * Github API stuff
          */
-        'GrahamCampbell\GitHub\GitHubServiceProvider',
+        GrahamCampbell\GitHub\GitHubServiceProvider::classgit,
     ],
 
     /*
@@ -198,7 +198,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'SocialAuth' => AdamWathan\EloquentOAuth\Facades\OAuth::class,
-        'GitHub' => 'GrahamCampbell\GitHub\Facades\GitHub',
+        'GitHub' => GrahamCampbell\GitHub\Facades\GitHub::class,
     ],
 
 ];

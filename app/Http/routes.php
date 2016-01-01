e75879a60260cf5fd503a6587f6938079e6b3b9c
Ben Redden <benjamminredden@gmail.com>
2016-01-01 17:35:47 -0500

    got rid of unneeded code [deploy: production]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -45,14 +45,14 @@ Route::get('/dashboard', function () {
         // The user is logged in...
         $user = Auth::user();
         // Get their issues
-        $issues = GitHub::issues()->all();
+        $issues = GitHub::issues();
         dd($issues);
 
         $data = array(
             'fullname' => $user->full_name,
             'nickname' => $user->nickname,
         );
-        return view('dashboard')->with($data)->with($issues);
+        return view('dashboard')->with($data);
     }
     else
     {

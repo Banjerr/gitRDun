4b544748199d6ea72552cc35c5f30aa271c8cd00
Ben Redden <benjamminredden@gmail.com>
2016-01-02 09:28:24 -0500

    more routes stuff [deploy: production]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,7 +52,7 @@ Route::get('/dashboard', function () {
         );
 
         // Get their issues
-        $issues = GitHub::repos()->currentUser()->all();
+        $issues = GitHub::currentUser();
         dd($issues);
 
 

d3e8813426358d173ef67d9e2a99a10b03c1d5de
Ben Redden <benjamminredden@gmail.com>
2016-01-02 11:18:50 -0500

    edits to home/dashboard views [deploy: production]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h2>Howdy <?php echo $nickname; ?></h2>
-    <img src="<?php echo $avatar; ?>" alt="<?php echo $nickname; ?>">
+    <img class="userAvatar" src="<?php echo $avatar; ?>" alt="<?php echo $nickname; ?>">
 @endsection

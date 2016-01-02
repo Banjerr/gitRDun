116764d01caff51009a1d4c29c6e55591345f521
Ben Redden <benjamminredden@gmail.com>
2016-01-02 09:33:09 -0500

    github api [deploy: production]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,7 +52,7 @@ Route::get('/dashboard', function () {
         );
 
         // Get their issues
-        $issues = GitHub::currentUser();
+        $issues = GitHub::getDefaultConnection();
         dd($issues);
 
 

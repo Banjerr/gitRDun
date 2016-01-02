68fb3d6ffd78b92a3b20a8d6439722705614f4bd
Ben Redden <benjamminredden@gmail.com>
2016-01-02 09:35:43 -0500

    github api again [deploy: production]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,7 +52,7 @@ Route::get('/dashboard', function () {
         );
 
         // Get their issues
-        $issues = GitHub::getDefaultConnection();
+        $issues = GitHub::getDefaultConnection()->currentUser()->repos()->all();
         dd($issues);
 
 

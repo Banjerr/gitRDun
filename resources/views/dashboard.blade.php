fca216e6b9c7e956543125d3afe18d22a1f2049d
Ben Redden <benjamminredden@gmail.com>
2016-01-03 11:41:29 -0500

    changing [deploy: production]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,18 +5,6 @@
     <img class="userAvatar" src="<?php echo $avatar; ?>" alt="<?php echo $nickname; ?>">
     <?php
     // if has_issues is true add it to the array
-    foreach($repos as $repo)
-    {
-        $withIssues = array();
-        dd($repo);
-        while($repo['has_issues'] == true)
-        {
-            echo $repo{full_name];
-            $withIssues['name'] = $repo['full_name'];
-            //array_push( $withIssues, $this );
-        }
-    }
-
-    print_r($withIssues);
+    echo $repos;
     ?>
 @endsection

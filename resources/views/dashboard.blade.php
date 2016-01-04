1cbc66b425a769a9a538b6b5261087cd9580f6cd
Ben Redden <benjamminredden@gmail.com>
2016-01-04 08:41:51 -0500

    stuff again [deploy: production]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,5 @@
 
 @section('content')
     <h2>Howdy <?php echo $data['nickname']; ?></h2>
-    {{--<img class="userAvatar" src="<?php echo $avatar; ?>" alt="<?php echo $nickname; ?>">--}}
-    {{--@if ($repos)--}}
-        {{--<?php--}}
-            {{--foreach ($repos as $repo)--}}
-            {{--{--}}
-                {{--while ($repo['full_name'])--}}
-                {{--{--}}
-                    {{--print_r($repo);--}}
-                {{--}--}}
-            {{--}--}}
-        {{--?>--}}
-    {{--@else--}}
-        {{--<p>No repos yet</p>--}}
-    {{--@endif--}}
+
 @endsection

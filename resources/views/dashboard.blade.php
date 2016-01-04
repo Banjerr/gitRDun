c2f3a70ba5bc3346587eb11bbf7410995a17cebd
Ben Redden <benjamminredden@gmail.com>
2016-01-03 22:05:55 -0500

    removed variable from blade template [deploy: production]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,4 @@
 @section('content')
     <h2>Howdy <?php echo $nickname; ?></h2>
     <img class="userAvatar" src="<?php echo $avatar; ?>" alt="<?php echo $nickname; ?>">
-    <?php
-    // if has_issues is true add it to the array
-    echo $repos;
-    ?>
 @endsection

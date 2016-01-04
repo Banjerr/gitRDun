3c559ca88b785ef616741dbe35874cd75409fd90
Ben Redden <benjamminredden@gmail.com>
2016-01-03 22:01:18 -0500

    route edits again [deploy: production]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -55,7 +55,7 @@ Route::get('/dashboard', function () {
 
         // Get the repos for the authenticated user
         $repos = GitHub::user()->repositories($data['nickname']);
-
+        dd($repos);
         return view('dashboard')->with($data)->with($repos);
     }
     else

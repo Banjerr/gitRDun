f484ed4d352c180859d0d9cbf3f57c7f80838aa2
Ben Redden <benjamminredden@gmail.com>
2016-01-03 22:31:30 -0500

    edit to blade [deploy: production]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,17 @@
 @section('content')
     <h2>Howdy <?php echo $nickname; ?></h2>
     <img class="userAvatar" src="<?php echo $avatar; ?>" alt="<?php echo $nickname; ?>">
-    <?php
-        print_r($repos);
-    ?>
+    @if ($repos)
+        <?php
+            foreach ($repos as $repo)
+            {
+                while ($repo['full_name'])
+                {
+                    echo $repo;
+                }
+            }
+        ?>
+    @else
+        <p>No repos yet</p>
+    @endif
 @endsection

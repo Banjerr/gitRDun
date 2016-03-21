28f95490c8da7a592e1f3b0951fb069977c4ad0a
Ben Redden <benjamminredden@gmail.com>
2016-03-20 21:35:53 -0400

    still debugging [deploy: production]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,17 +6,17 @@
 
     <?php dd($repos); ?>
 
-    @if ($repos)
-        <?php
-        foreach ($repos as $repo)
-        {
-            while ($repo['full_name'])
-            {
-                print_r($repo);
-            }
-        }
-        ?>
-    @else
+    {{--@if ($repos)--}}
+        {{--<?php--}}
+        {{--foreach ($repos as $repo)--}}
+        {{--{--}}
+            {{--while ($repo['full_name'])--}}
+            {{--{--}}
+                {{--print_r($repo);--}}
+            {{--}--}}
+        {{--}--}}
+        {{--?>--}}
+    {{--@else--}}
         <p>No repos yet</p>
     @endif
 @endsection

9a9fab5a94a9fbd8b250cfaeb85fa49f6ac823e3
Ben Redden <benjamminredden@gmail.com>
2016-03-20 21:19:57 -0400

    debugging variables [deploy: production]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <h2>Howdy {{ $nickname }}</h2>
     <img class="userAvatar" src="{{ $avatar }}" alt="{{ $nickname }}">
+
+    <?php dd($repos); ?>
+
     @if ($repos)
         <?php
         foreach ($repos as $repo)

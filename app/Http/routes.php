0abd75a5ebc421fb4581b294b569160e7ed994fe
Ben Redden <benjamminredden@gmail.com>
2016-03-20 21:45:03 -0400

    still debugging [deploy: production]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -56,7 +56,7 @@ Route::get('/dashboard', function () {
         // Get the repos for the authenticated user
         $repos = GitHub::user()->repositories($user->nickname);
 
-        return view('dashboard')->with([$data, $repos]);
+        return view('dashboard')->with(['data' => $data, 'repos' => $repos]);
     }
     else
     {
@@ -130,7 +130,7 @@ Route::get('github/login', function() {
     // Get the repos for the authenticated user
     $repos = GitHub::user()->repositories($user->nickname);
 
-    return view('dashboard')->with([$data, $repos]);
+    return view('dashboard')->with(['data' => $data, 'repos' => $repos]);
 });
 
 /**
